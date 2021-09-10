73e3b7bb79be4a835dbbf8639af981d3eac5aca1
0alif <alifhossain003@gmail.com>
2021-09-11 00:59:28 +0600

    clear

--- object.ts
+++ object.ts
@@ -1,15 +1,26 @@
 const city: string = "Dhaka";
 const fruits: string[] = ["mango", "banana", "apple"];
 
-type person = {name: string; age: number; salary: number}
+type person = { name: string; age: number; salary: number }
 
 const student: person = {
   name: "Kamal Atartuk",
   age: 115,
   salary: 4500,
 };
-const emploie: person ={
+const emploie: person = {
   name: 'Solaiman',
   age: 29,
   salary: 1000
+}
+
+function getSalary(player: { name: string, salary: number }): number {
+  return player.salary;
+}
+
+getSalary({ name: 'Kuddus', salary: 50 });
+
+
+function getSalary2(player: person): number {
+  return player.salary;
 }
